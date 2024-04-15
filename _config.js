20124b4ac6fc901f833206417c60e88606031aab
joashtoo <toojoash@gmail.com>
2024-04-15 16:56:14 +0300

    added launch.json to run script

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
-    development: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
-    test: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
+    production: 'mongodb+srv://joashtoo:<bandlife1234>@cluster0.orndeoe.mongodb.net/',
+    development: 'mongodb+srv://joashtoo:<bandlife1234>@cluster0.orndeoe.mongodb.net/',
+    test: 'mongodb+srv://joashtoo:<bandlife1234>@cluster0.orndeoe.mongodb.net/',
 }
 module.exports = config;
