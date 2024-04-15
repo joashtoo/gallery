fb88f15fbbcb2c6919c0e94100041ee7e3e08a1b
joashtoo <toojoash@gmail.com>
2024-04-15 15:37:06 +0300

    username and password change to mongodb

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<JOASH TOO>:<bandlife1234>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<JOASH TOO>:<bandlife1234>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<JOASH TOO>:<bandlife1234>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
+    development: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
+    test: 'mongodb+srv://joashtoo:<password>@cluster0.orndeoe.mongodb.net/',
 }
 module.exports = config;
